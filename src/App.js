399295ebfede5eac730a08220c3498b3a182b138
EC2 Default User <ec2-user@ip-172-31-80-235.ec2.internal>
2018-07-15 07:10:49 +0000

    initial checkin

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Header } from 'semantic-ui-react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <Header as="h1" size="huge"> Hi, Max </Header>
+      </div>
+    );
+  }
+}
+
+export default App;
