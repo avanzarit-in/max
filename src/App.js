a10b31208ec21fdc62d8fd12baa87637128b0c52
EC2 Default User <ec2-user@ip-172-31-80-235.ec2.internal>
2018-07-15 09:45:34 +0000

    some changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { Header } from 'semantic-ui-react';
+import Menu from './Menu/menu';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Menu/>
        <Header as="h1" size="huge"> Hi, Max </Header>
       </div>
     );
