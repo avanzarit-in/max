bd7ed5bc2347a4f4dea76aef6e13a4a2d3b5806a
Santosh_Padhi <santosh.padhi@philips.com>
2018-09-21 12:21:21 +0530

    some changes

--- src/component/containers/Landing.js
+++ src/component/containers/Landing.js
@@ -1,17 +1,80 @@
 import React, { Component } from 'react';
-import { Card, Icon, Image, Button, Grid, Label } from 'semantic-ui-react';
+import { Card, Icon, Image, Button, Grid, Label ,Dimmer,Loader} from 'semantic-ui-react';
 import Background from './../../image/128-216.jpg';
 import NumberFormat from 'react-number-format'
 import moment from 'moment'
 import { Link } from 'react-router-dom'
+import Api from './../utils/Api'
 
 export default class Landing extends Component {
+  constructor(props) {
+        super(props);
+        this.state = {
+            fromDate: moment([2017, 3, 1]),
+            toDate: moment(),
+            paymentDue: 0,
+            paymentReceived: 0,
+            dataloaded:false
+        }
+    }
+
+    calculateAmounts = (data) => {
+        let paymentDue = 0;
+        let paymentReceived = 0;
+        let result = {};
+
+        if (data !== undefined) {
+            let payload = data.filter(item => {
+
+                if (item.CD === undefined || item.CD === "") {
+                    paymentDue = paymentDue + parseInt(item.D);
+                    paymentReceived = paymentReceived + parseInt(item.C);
+                    result.paymentDue = paymentDue;
+                    result.paymentReceived = paymentReceived;
+
+                    return true;
+                }
+
+                return false;
+            })
+        }
+
+        return result;
+    }
+
+    componentDidMount() {
+        let fromDate = this.state.fromDate.format('DD.MM.YYYY');
+        let toDate = this.state.toDate.format('DD.MM.YYYY');
+        console.log("component did mount called");
+        let statementData = "";
+        Api.getCustomer(this.props.username).then(userDisplayName => {
+            let displayName = userDisplayName;
+            console.log(displayName);
+            Api.fetchStatementData(this.props.username, "summary", fromDate, toDate).then(statementData => {
+                console.log(statementData);
+                let result = this.calculateAmounts(statementData);
+                this.setState(
+                    {
+                        paymentDue: result.paymentDue,
+                        paymentReceived: result.paymentReceived,
+                        dataloaded:true
+                    });
+            }, error => {
+                console.log("ERROR ==>" + error.type);
+            })
+
+        }, error => {
+            console.log("ERROR ==>" + error.type);
+        })
+    }
 
   render() {
 
     let today = moment().format("DD/MM/YYYY");
+    let netOutstanding = parseInt(this.state.paymentDue) - parseInt(this.state.paymentReceived);
 
     return (
+      this.state.dataloaded ?
       <Card.Group itemsPerRow={2} style={{ marginTop: '100px', marginLeft: '200px', marginRight: '200px' }}>
         {/**
         <Card>
@@ -86,7 +149,7 @@ export default class Landing extends Component {
                     Payment Received:
                   </Grid.Column>
                   <Grid.Column>
-                    <NumberFormat prefix={'Rs.'} value={1243445456} displayType={'text'} thousandSeparator={true} decimalScale={2} fixedDecimalScale />
+                    <NumberFormat prefix={'Rs.'} value={this.state.paymentReceived} displayType={'text'} thousandSeparator={true} decimalScale={2} fixedDecimalScale />
                   </Grid.Column>
                 </Grid.Row>
                 <Grid.Row style={{ paddingBottom: '0px' }}>
@@ -94,7 +157,7 @@ export default class Landing extends Component {
                     Payment Due:
                   </Grid.Column>
                   <Grid.Column>
-                    <NumberFormat prefix={'Rs.'} value={1243445456} displayType={'text'} thousandSeparator={true} decimalScale={2} fixedDecimalScale />
+                    <NumberFormat prefix={'Rs.'} value={this.state.paymentDue} displayType={'text'} thousandSeparator={true} decimalScale={2} fixedDecimalScale />
                   </Grid.Column>
                 </Grid.Row>
                 <Grid.Row >
@@ -102,7 +165,7 @@ export default class Landing extends Component {
                     Total Outstanding:
                   </Grid.Column>
                   <Grid.Column>
-                    <NumberFormat prefix={'Rs.'} value={1243445456} displayType={'text'} thousandSeparator={true} decimalScale={2} fixedDecimalScale />
+                    <NumberFormat prefix={'Rs.'} value={netOutstanding} displayType={'text'} thousandSeparator={true} decimalScale={2} fixedDecimalScale />
                   </Grid.Column>
                 </Grid.Row>
               </Grid>
@@ -165,6 +228,10 @@ export default class Landing extends Component {
           </Card.Content>
         </Card> **/}
       </Card.Group>
+       :
+            <Dimmer active inverted>
+                    <Loader size='large'>Loading</Loader>
+                </Dimmer>
     );
   }
 }
